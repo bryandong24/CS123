dba5573d95c08c94dff94c21c0e2cab6254714f9
Bryan Dong <bryandong24@gmail.com>
2024-09-30 17:21:28 -0700

    Added Correct Translations and Rotations

--- lab_2/lab_2.py
+++ lab_2/lab_2.py
@@ -71,18 +71,20 @@ class ForwardKinematics(Node):
                 [0, 0, 1, z],
                 [0, 0, 0, 1]
             ])
+        
+        # RGB -> x,y,z
 
         # T_0_1 (base_link to leg_front_r_1)
         T_0_1 = translation(0.07500, -0.0445, 0) @ rotation_x(pi/2) @ rotation_z(theta1)
 
         # T_1_2 (leg_front_r_1 to leg_front_r_2)
-        T_1_2 = translation(0, 0, 0) @ rotation_y(theta2)
+        T_1_2 = translation(0, 0, 0.00039) @ rotation_y(3*pi/2) @ rotation_z(theta2)
 
         # T_2_3 (leg_front_r_2 to leg_front_r_3)
-        T_2_3 = translation(0.1, 0, 0) @ rotation_y(theta3)
+        T_2_3 = translation(0, -0.0494, 0.0685) @ rotation_y(pi/2) @rotation_z(theta3)
 
         # T_3_ee (leg_front_r_3 to end-effector)
-        T_3_ee = translation(0.1, 0, 0)
+        T_3_ee = translation(0.06231, 0, 0.018)
 
         # Compute the final transformation
         T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
